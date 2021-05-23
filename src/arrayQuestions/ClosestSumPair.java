b3890b7b121b0ed7c8ba8ac25cd6fa0741f3b818
erdemcicek <79759792+erdemcicek@users.noreply.github.com>
2021-05-23 14:32:43 +0200

    Update ClosestSumPair.java

--- src/arrayQuestions/ClosestSumPair.java
+++ src/arrayQuestions/ClosestSumPair.java
@@ -1,14 +1,14 @@
 package arrayQuestions;
 
 /*
-			You are given two int arrays and a target integer.
-			Write a program to find the closest pair to the target
+	You are given two int arrays and a target integer.
+	Write a program to find the closest pair to the target
 			
-			int[] a1 = {3, 4, 7, 11}; 
-        	int[] a2 = {6, 14, 9, 5}; 
-        	int target = 26;
+	int[] a1 = {3, 4, 7, 11}; 
+        int[] a2 = {6, 14, 9, 5}; 
+        int target = 26;
         	
-        	Output : [11, 14]
+        Output : [11, 14]
  */
 
 import java.util.Arrays;
@@ -17,7 +17,7 @@ public class ClosestSumPair {
 	
 	public static void main(String[] args) {
 		
-		int[] a1 = {3, 4, 7, 11};  
+	int[] a1 = {3, 4, 7, 11};  
         int[] a2 = {6, 14, 9, 5}; 
         int aTarget = 26;
         System.out.println(Arrays.toString(closestSumPair(a1, a2, aTarget)));

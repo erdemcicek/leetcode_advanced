a7dba06623c9553df28216ae7034fb7fad8e3a03
erdemcicek <79759792+erdemcicek@users.noreply.github.com>
2021-05-27 22:54:27 +0200

    Update gridTraveler.java

--- src/recursion/gridTraveler.java
+++ src/recursion/gridTraveler.java
@@ -11,12 +11,11 @@ public class gridTraveler {
 	}
 	public static long gridTravel(int m, int n, Map<String, Long> hm) {
 
-		String s = m + "," + n;
+		String s = m + "," + n; // this will create unique key for each path
 		if ( hm.containsKey(s)) return hm.get(s); 
 		if ( m == 1 && n == 1) return 1;
 		if ( m == 0 || n == 0) return 0;
 		hm.putIfAbsent(s, gridTravel(m-1, n, hm) + gridTravel(m, n-1, hm));
 		return hm.get(s);
 	}
-
 }

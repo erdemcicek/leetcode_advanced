178066f9407787abf13259350d6500da995f52c5
erdemcicek <79759792+erdemcicek@users.noreply.github.com>
2021-06-04 17:12:37 +0200

    Update ReverseInteger.java

--- src/other/ReverseInteger.java
+++ src/other/ReverseInteger.java
@@ -6,11 +6,10 @@ public class ReverseInteger {
 		
 //		System.out.println(reverse(2,147,483,647));// 3021									
 //		System.out.println(reverseWithString(Integer.MAX_VALUE));
-//		System.out.println(Integer.MAX_VALUE);
 		System.out.println(reverseWithStringTryCatch(2047483699));
 		
 	}
-	public static int reverse(int x) {
+	private static int reverse(int x) {
 		
 		int n = 0, n_rev = 0, t = 0, x_in = x;
 		
@@ -22,13 +21,13 @@ public class ReverseInteger {
 		
 		t = n;// 3021
 		
-		while(t != 0) {
+		while( t != 0 ) {
 			
 			n_rev = n_rev*10 + t%10; // 0 + 1 , 10 + 2 , 120 + 0 , 1200 + 3 
 			t = t/10;				 //  302  ,   30   ,   3     ,   0
 		}
 		
-		while(x_in%10==0 && x_in!=0) {
+		while( x_in % 10 == 0 && x_in != 0) {
 			
 			x_in = x_in/10;
 		}
@@ -38,7 +37,7 @@ public class ReverseInteger {
 		return 0;
 	}
 	
-	public static int reverseWithString(int x) { 
+	private static int reverseWithString(int x) { 
 
 		boolean isNeg = (x<0)?(true):(false);
 		if ( x == Integer.MIN_VALUE) return 0;
@@ -50,7 +49,7 @@ public class ReverseInteger {
 		return (isNeg)?(-1 * Integer.parseInt(sb.toString())) : ( Integer.parseInt(sb.toString()));
 	}
 	
-	public static int reverseWithStringTryCatch(int x) {
+	private static int reverseWithStringTryCatch(int x) {
 		
 		String s = (x<0)?(  new StringBuilder(String.valueOf(-x)).reverse().toString())
 				: ( new StringBuilder(String.valueOf(x)).reverse().toString() );

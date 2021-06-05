39c71d7a93f40528a6d170eae73da35a3f9f58d1
erdemcicek <79759792+erdemcicek@users.noreply.github.com>
2021-06-05 22:42:36 +0200

    Update IntegerToRoman.java

--- src/stringManipulation/IntegerToRoman.java
+++ src/stringManipulation/IntegerToRoman.java
@@ -4,10 +4,10 @@ import java.util.List;
 
 public class IntegerToRoman {
 	
-	public static void main(String[] args) { // 1 3999
+	public static void main(String[] args) { 
 		
-//		System.out.println(intToRoman(394));
-		System.out.println(convertRoman(213)); // 547
+		System.out.println(intToRoman(394));
+		System.out.println(convertRoman(213)); 
 	}
 	
 	final static int[] val = {1000,900,500,400,100,90,50,40,10,9,5,4,1}; // I II III IV V VIII IX
@@ -34,12 +34,6 @@ public class IntegerToRoman {
 		StringBuilder sb = new StringBuilder("" + n); 
 		sb.reverse();
 		StringBuilder t = new StringBuilder();
-		/*			 1111               444                 777
-		 		i=0  1		ind=0 2i	4  		ind=0,1		7	  ind=1,0(mth)  2i+1,2i(mth)
-		 		i=1  10		ind=2 2i	40		ind=2,3		70 	  ind=3,2(mth)  2i+1,2i(mth)
-		 		i=2  100	ind=4 2i	400		ind=4,5 	700	  ind=5,4(mth)  2i+1,2i(mth)
-		 		i=3  1000	ind=6 2i	----				----
-		 */
 		
 		for ( int i = sb.length()-1 ; i >= 0  ; i--) {			
 			int digit = sb.charAt(i) - '0'; 

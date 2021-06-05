8beec3f63efc254cf06246a6ffb7ed06298644f1
erdemcicek <79759792+erdemcicek@users.noreply.github.com>
2021-06-05 22:40:39 +0200

    Update MaxWaterContainer.java

--- src/arrayQuestions/MaxWaterContainer.java
+++ src/arrayQuestions/MaxWaterContainer.java
@@ -5,11 +5,8 @@ import java.util.Arrays;
 public class MaxWaterContainer {
 	
 	public static void main(String[] args) {
-		
-		
 		int[] heights = {1, 8, 6, 2, 5, 4, 8, 3, 7};
 		System.out.println(maxArea(heights));
-		
 	}
 	public static int maxArea(int[] heights) {
 		

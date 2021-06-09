eacb98cae2a72030495110305534fcf218d97383
erdemcicek <79759792+erdemcicek@users.noreply.github.com>
2021-06-09 21:36:48 +0200

    Update OccurrenceOfLetters.java

--- src/arrayQuestions/OccurrenceOfLetters.java
+++ src/arrayQuestions/OccurrenceOfLetters.java
@@ -6,12 +6,12 @@ public class OccurrenceOfLetters {
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		String str = scan.next(); // tablet
-		String[] s = str.split(""); // 			t,a,b,l,e,t
-		int[] count = new int[str.length()]; // 0,0,0,0,0,0
+		String str = scan.next();
+		String[] s = str.split("");
+		int[] count = new int[str.length()];
 		for ( int i = 0 ; i < str.length() ; i++) {	
 			if ( str.contains(s[i])) {
-				count[str.indexOf(s[i])]++;  // String'deki indexOf baz alinmali*** 
+				count[str.indexOf(s[i])]++;
 			}
 		}	
 		for ( int i = 0 ; i<str.length(); i++){

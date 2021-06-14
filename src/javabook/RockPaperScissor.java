996383c6a1af4fc72d040e0ec2e02c916ebb3d01
erdemcicek <79759792+erdemcicek@users.noreply.github.com>
2021-06-14 04:18:23 +0200

    Update RockPaperScissor.java

--- src/javabook/RockPaperScissor.java
+++ src/javabook/RockPaperScissor.java
@@ -3,8 +3,9 @@ package javabook;
 import java.util.Scanner;
 
 public class RockPaperScissor {
+	
 	/*
-	 	*(Game: scissor, rock, paper) Write a program that plays the popular scissor–rock–
+	 	*(Game: scissor, rock, paper) Write a program that plays the popular scissorâ€“rockâ€“
 		paper game. (A scissor can cut a paper, a rock can knock a scissor, and a paper can
 		wrap a rock.) The program randomly generates a number 0, 1, or 2 representing
 		scissor, rock, and paper. The program prompts the user to enter a number 0, 1, or
@@ -17,7 +18,6 @@ public class RockPaperScissor {
 		scissor (0), rock (1), paper (2): 2
 		The computer is paper. You are paper too. It is a draw
 	  
-	 
 	 	*(Game: scissor, rock, paper) Programming Exercise 3.17 gives a program that plays
 		the scissor-rock-paper game. Revise the program to let the user continuously play
 		until either the user or the computer wins three times more than their opponent.
@@ -26,8 +26,8 @@ public class RockPaperScissor {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		int countUserWin = 0 , countComWin = 0;
-//		while ( countUserWin < 5 && countComWin < 5) {
-		while ( Math.abs(countUserWin - countComWin) < 3 ) {
+		while ( countUserWin < 5 && countComWin < 5) {
+//		while ( Math.abs(countUserWin - countComWin) < 3 ) {
 			System.out.println("Enter from 0 to 2 -> scissor (0), rock (1), paper (2):");
 			int user = scan.nextInt();
 			int com = (int)(Math.random() * 3);

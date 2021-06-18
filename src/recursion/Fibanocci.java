eb1b4051eeae1b13af98b0b5d60d4763d840073f
erdemcicek <79759792+erdemcicek@users.noreply.github.com>
2021-06-18 23:37:35 +0200

    Update Fibanocci.java

--- src/recursion/Fibanocci.java
+++ src/recursion/Fibanocci.java
@@ -5,9 +5,8 @@ public class Fibanocci {
 	public static void main(String[] args) {
 		
 		System.out.println(fib(6));
-		
 	}
-	public static long fib(int n) { // 1,1,2,3
+	private static long fib(int n) { // 1,1,2,3
 		
 		long[] arr = new long[n+1];
 		
@@ -17,10 +16,9 @@ public class Fibanocci {
 		for ( int i = 3 ; i <= n ; i++) {
 			arr[i] = arr[i-1] + arr[i-2];
 		}
-		
 		return arr[n];
 	}
-	public static long fibRec(int n) {
+	private static long fibRec(int n) {
 		
 		if ( n == 1 || n == 2) return 1;
 		

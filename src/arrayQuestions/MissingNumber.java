1e8a9d23d7083a6501a9e26a3095856056bf7d28
erdemcicek <79759792+erdemcicek@users.noreply.github.com>
2021-06-23 18:37:44 +0200

    Update MissingNumber.java

--- src/arrayQuestions/MissingNumber.java
+++ src/arrayQuestions/MissingNumber.java
@@ -10,7 +10,6 @@ public class MissingNumber {
 		int[] brr = {9,6,4,2,3,5,7,0,1};
 		System.out.println(missingNum(brr));
 		System.out.println(missingNum2(brr));
-		
 	}
 	private static int missingNum(int[] nums) {
 		
@@ -21,13 +20,10 @@ public class MissingNumber {
 			if ( nums[i] != x) found = false;
 			x++;
 			if (!found) return nums[i]-1;
-			
 		}
 		if (found) return nums.length;
-		
 		return 0;	
 	}
-	
 	private static int missingNum2(int[] nums) {
 		int len = nums.length;
 		int sum = (len*(len+1))/2;

bca8ed0bf83fb47700de2904bde4d30d874e5de4
erdemcicek <79759792+erdemcicek@users.noreply.github.com>
2021-07-02 11:46:14 +0200

    Update PalindromeNumber.java

--- src/stringManipulation/PalindromeNumber.java
+++ src/stringManipulation/PalindromeNumber.java
@@ -1,14 +1,12 @@
 package stringManipulation;
 
-
 public class PalindromeNumber {
 	
 	public static void main(String[] args) {
 		
 		System.out.println(isPalindrome(124));
-		
 	}
-	public static boolean isPalindrome(int n) {
+	private static boolean isPalindrome(int n) {
 		
 		if ( n < 0 ) return false;
 		else if ( n < 10 ) return true;
@@ -21,14 +19,3 @@ public class PalindromeNumber {
 		return true;
 	}
 }
-
-
-//else {
-//	int m = n;
-//	StringBuilder sb = new StringBuilder();
-//	while(n!=0) {
-//		sb.append(n%10);
-//		n /= 10;
-//	}
-//	if ( sb.toString().equals(""+m)) return true;	
-//}

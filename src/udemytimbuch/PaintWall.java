cb8556279ed43cc51082907bc597112e40b2152f
erdemcicek <79759792+erdemcicek@users.noreply.github.com>
2021-08-19 00:07:35 +0200

    Update PaintWall.java

--- src/udemytimbuch/PaintWall.java
+++ src/udemytimbuch/PaintWall.java
@@ -1,9 +1,9 @@
 package udemytimbuch;
 
 /*
- 		Bob is a wall painter and he needs your help. You have to write a program that helps Bob 
- 		calculate how many buckets of paint he needs to buy before going to work. Bob might also 
- 		have some extra buckets at home. He also knows the area that he can cover with one bucket of paint.
+ 	Bob is a wall painter and he needs your help. You have to write a program that helps Bob 
+ 	calculate how many buckets of paint he needs to buy before going to work. Bob might also 
+ 	have some extra buckets at home. He also knows the area that he can cover with one bucket of paint.
 
 1. Write a method named getBucketCount with 4 parameters. The first parameter should be named width of type double. 
 This parameter represents the width of the wall.
@@ -45,20 +45,16 @@ public class PaintWall {
         double wallArea = width * height;
         double alreadyHave = areaPerBucket * extraBuckets;
         double req = wallArea - alreadyHave;
-        return (int) Math.ceil(req/areaPerBucket);
-        
+        return (int) Math.ceil(req/areaPerBucket);        
     }
     private static int getBucketCount(double width, double height, double areaPerBucket){
         if(width<=0 || height<=0 || areaPerBucket<=0) return -1;
         double wallArea = width * height;
         
         return (int) Math.ceil(wallArea/areaPerBucket);
-        
     }
     private static int getBucketCount(double area, double areaPerBucket){
         if(area<=0 || areaPerBucket<=0) return -1;
         return (int) Math.ceil(area/areaPerBucket);
     }
-	
-
 }

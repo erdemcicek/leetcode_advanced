1bb9d4bd4db6442950160a5e60cff0129d9bc41b
erdemcicek <79759792+erdemcicek@users.noreply.github.com>
2021-08-20 23:40:50 +0200

    Update PaintWall.java

--- src/udemytimbuch/PaintWall.java
+++ src/udemytimbuch/PaintWall.java
@@ -40,21 +40,25 @@ public class PaintWall {
 	public static void main(String[] args) {
 		System.out.println(Math.ceil(2.01));
 	}
+	
 	private static int getBucketCount(double width,double height,double areaPerBucket,int extraBuckets){
-        if(width<=0 || height<=0 || areaPerBucket<=0 || extraBuckets<0) return -1;
-        double wallArea = width * height;
-        double alreadyHave = areaPerBucket * extraBuckets;
-        double req = wallArea - alreadyHave;
-        return (int) Math.ceil(req/areaPerBucket);        
-    }
-    private static int getBucketCount(double width, double height, double areaPerBucket){
-        if(width<=0 || height<=0 || areaPerBucket<=0) return -1;
-        double wallArea = width * height;
+		
+        	if(width<=0 || height<=0 || areaPerBucket<=0 || extraBuckets<0) return -1;
+        	double wallArea = width * height;
+        	double alreadyHave = areaPerBucket * extraBuckets;
+        	double req = wallArea - alreadyHave;
+        	return (int) Math.ceil(req/areaPerBucket);        
+    	}
+    	private static int getBucketCount(double width, double height, double areaPerBucket){
+		
+        	if(width<=0 || height<=0 || areaPerBucket<=0) return -1;
+        	double wallArea = width * height;
         
-        return (int) Math.ceil(wallArea/areaPerBucket);
-    }
-    private static int getBucketCount(double area, double areaPerBucket){
-        if(area<=0 || areaPerBucket<=0) return -1;
-        return (int) Math.ceil(area/areaPerBucket);
-    }
+        	return (int) Math.ceil(wallArea/areaPerBucket);
+    	}
+    	private static int getBucketCount(double area, double areaPerBucket){
+		
+        	if(area<=0 || areaPerBucket<=0) return -1;
+        	return (int) Math.ceil(area/areaPerBucket);
+    	}
 }

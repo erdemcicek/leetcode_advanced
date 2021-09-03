9324c1daea6f16ce3e5ec6d3887c03e34d877c19
erdemcicek <erdem25123@gmail>
2021-09-03 14:14:11 +0200

    valid parenthesis review

--- src/other2/ValidParenthesis.java
+++ src/other2/ValidParenthesis.java
@@ -13,21 +13,16 @@ public class ValidParenthesis {
 	}
 
 	private static boolean valid(char b, char e) {
-        	return b == '(' && e == ')' || b == '{' && e == '}' || b == '[' && e == ']';
-    	}
+        return b == '(' && e == ')' || b == '{' && e == '}' || b == '[' && e == ']';
+    }
 
-    	private static boolean isValid(String s) {
-        	Deque<Character> stack = new ArrayDeque<>();
-        	for (Character c : s.toCharArray()) {
-            	if (stack.isEmpty()) {
-                	stack.push(c);
-            	} else if (valid(stack.peek(), c)) {
-                	stack.pop();
-            	} else {
-                	stack.push(c);
-            	}
-        	}
-        
-        	return stack.isEmpty();  
-    	}
+	private static boolean isValid(String s) {
+		Deque<Character> stack = new ArrayDeque<>();
+		for (Character c: s.toCharArray()) {
+			if(stack.isEmpty()) stack.push(c);
+			else if(valid(stack.peek(), c)) stack.pop();
+			else stack.push(c);
+		}
+		return stack.isEmpty();
+	}
 }

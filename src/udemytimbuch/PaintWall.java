22d783aa4c0c9aaade10f268b59039557ce6e243
erdemcicek <79759792+erdemcicek@users.noreply.github.com>
2021-10-02 16:22:29 +0200

    Update PaintWall.java

--- src/udemytimbuch/PaintWall.java
+++ src/udemytimbuch/PaintWall.java
@@ -38,17 +38,21 @@ before returning the value from the methods.
 public class PaintWall {
 	
 	public static void main(String[] args) {
+		
 		System.out.println(Math.ceil(2.01));
 	}
 	
 	private static int getBucketCount(double width,double height,double areaPerBucket,int extraBuckets){
 		
         	if(width<=0 || height<=0 || areaPerBucket<=0 || extraBuckets<0) return -1;
+		
         	double wallArea = width * height;
         	double alreadyHave = areaPerBucket * extraBuckets;
         	double req = wallArea - alreadyHave;
+		
         	return (int) Math.ceil(req/areaPerBucket);        
     	}
+	
     	private static int getBucketCount(double width, double height, double areaPerBucket){
 		
         	if(width<=0 || height<=0 || areaPerBucket<=0) return -1;
@@ -56,6 +60,7 @@ public class PaintWall {
         
         	return (int) Math.ceil(wallArea/areaPerBucket);
     	}
+	
     	private static int getBucketCount(double area, double areaPerBucket){
 		
         	if(area<=0 || areaPerBucket<=0) return -1;

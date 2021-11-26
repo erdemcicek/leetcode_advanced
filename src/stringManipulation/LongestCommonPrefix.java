4e4625660fcd306f27864847f630320d1de1231a
erdemcicek <79759792+erdemcicek@users.noreply.github.com>
2021-11-26 19:27:42 +0100

    Update LongestCommonPrefix.java

--- src/stringManipulation/LongestCommonPrefix.java
+++ src/stringManipulation/LongestCommonPrefix.java
@@ -37,6 +37,7 @@ public class LongestCommonPrefix {
 		
 		String min = Arrays.stream(strs).min(Comparator.comparing(String::length)).get(); // flow
 		boolean cont = true;
+		
 		for ( int i = 0 ; i < min.length() ; i++) {
 			
 			for ( int j = 0 ; j < strs.length-1 ; j++ ) {
@@ -54,21 +55,25 @@ public class LongestCommonPrefix {
 	private static String longestCommonPrefix2(String[] a) {
 		
 	    int size = a.length;
+		
 	    /* if size is 0, return empty string */
-	    if (a.length == 0)
-	      return "";
-	    if (a.length == 1)
-	      return a[0];
+	    if (a.length == 0) return "";
+	    if (a.length == 1) return a[0];
+		
 	    /* sort the array of strings */
 	    Arrays.sort(a); 
+		
 	    /* find the minimum length from first and last string */
 	    int end = Math.min(a[0].length(), a[size-1].length()); 
+		
 	    /* find the common prefix between the first and
 	      last string */
 	    int i = 0;
 	    while (i < end && a[0].charAt(i) == a[size-1].charAt(i) )
 	      i++;
+		
 	    String pre = a[0].substring(0, i);
+		
 	    return pre;
 	  }
 }

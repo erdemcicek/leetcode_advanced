fd62cba96d2ad463c44a8a77c5b4798da0d995fd
erdemcicek <79759792+erdemcicek@users.noreply.github.com>
2021-11-29 23:56:02 +0100

    Update MaxNumber.java

--- src/other2/MaxNumber.java
+++ src/other2/MaxNumber.java
@@ -9,10 +9,13 @@ public class MaxNumber {
 	public static void main(String[] args) {
 		System.out.println(findMaxCombin("1573935"));
 	}
+	
 	private static String findMaxCombin(String s){
+		
 		List<String> list = new ArrayList<>();
 		StringBuilder sb = new StringBuilder();
 		int p = 0;
+		
 		for(int i = 0 ; i < s.length() - 1 ; i++) {
 			if((s.charAt(i) - s.charAt(i+1)) % 2 == 0) sb.append(s.charAt(i));
 			else {
@@ -26,13 +29,17 @@ public class MaxNumber {
 		list.add(s.substring(p+1, s.length()));
 		System.out.println(list);
 		StringBuilder acc = new StringBuilder();
+		
 		for(int i = 0 ; i < list.size() ; i++) {
 			if(list.get(i).length() > 1) acc.append(sortPartial(list.get(i)));
 			else acc.append(list.get(i));
 		}
+		
 		return acc.toString();
 	}
+	
 	private static String sortPartial(String s) {
+		
 		char[] arr = s.toCharArray();
 		Arrays.sort(arr);
 		StringBuilder sbb = new StringBuilder();

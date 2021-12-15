1fea9d466cc0489825e49e96e0a7dd602ed3bd16
erdemcicek <79759792+erdemcicek@users.noreply.github.com>
2021-12-15 14:21:11 +0100

    Update CardGame.java

--- src/other2/CardGame.java
+++ src/other2/CardGame.java
@@ -5,6 +5,7 @@ import java.util.List;
 public class CardGame {
 	
 	public static void main(String[] args) {
+		
 		System.out.println(findWinScore("5AKQ4", "3KT81"));
 	}
 	
@@ -12,9 +13,11 @@ public class CardGame {
 		
 		List<Character> deck = List.of('2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A');
 		int score = 0;
+		
 		for(int i = 0; i < s.length(); i++) {
 			if(deck.indexOf(s.charAt(i)) > deck.indexOf(t.charAt(i))) score++;
-		}		
+		}
+		
 		return score;
 	}
 

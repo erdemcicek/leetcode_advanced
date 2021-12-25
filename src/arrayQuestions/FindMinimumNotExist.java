0cc85743a983167e1918019c3fa6a61014d059fb
erdemcicek <79759792+erdemcicek@users.noreply.github.com>
2021-12-26 00:12:37 +0100

    Update FindMinimumNotExist.java

--- src/arrayQuestions/FindMinimumNotExist.java
+++ src/arrayQuestions/FindMinimumNotExist.java
@@ -14,15 +14,20 @@ public class FindMinimumNotExist {
 	}
 	
 	private static int solution(int[] A) {
+		
 		if(A==null) return 1;
+		
 		Arrays.sort(A);
 		int[] arr = Arrays.stream(A).distinct().filter(t->t>0).toArray();
+		
 		if(arr.length==0) return 1;
 		int p = 1;
+		
 		for(int i = 0 ; i < arr.length ; i++) {
 			if(p == arr[i]) p++;
 			else return p;
 		}
+		
 		return arr[arr.length-1] + 1;
 	}
 }

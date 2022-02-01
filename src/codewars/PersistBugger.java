cdc3d2cd2764eb5e53d8bc509e8840c7e3dd67e4
erdemcicek <79759792+erdemcicek@users.noreply.github.com>
2022-02-01 20:29:08 +0100

    Update PersistBugger.java

--- src/codewars/PersistBugger.java
+++ src/codewars/PersistBugger.java
@@ -3,9 +3,13 @@ package codewars;
 public class PersistBugger {
 	
 	public static void main(String[] args) {
+		
 		System.out.println(persistence(25));
+		
 	}
+	
 	private static int persistence(long n) { // 39
+		
 		int pers = 0;
 		while(n>9) {
 			n = findCurrentProd(n);
@@ -13,7 +17,9 @@ public class PersistBugger {
 		}
 		return pers;
 	}
+	
 	private static int findCurrentProd(long n) {
+		
 		if(n==0) return 1;
 		int prod = 1;
 		while(n!=0) {

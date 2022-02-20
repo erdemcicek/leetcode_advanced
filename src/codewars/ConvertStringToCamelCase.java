11812e49e7bb9cdd94e933e25821c49aadb65c1a
erdemcicek <79759792+erdemcicek@users.noreply.github.com>
2022-02-20 01:08:31 +0100

    Update ConvertStringToCamelCase.java

--- src/codewars/ConvertStringToCamelCase.java
+++ src/codewars/ConvertStringToCamelCase.java
@@ -5,6 +5,7 @@ public class ConvertStringToCamelCase {
 	public static void main(String[] args) {
 		
 		System.out.println(toCamelCase("the-Stealth-Warrior"));
+		
 	}
 	
 	private static String toCamelCase(String s) { // the_stealth_warrior
@@ -12,6 +13,7 @@ public class ConvertStringToCamelCase {
 		var sb = new StringBuilder(s);
 		
 		for(int i = 0 ; i < sb.length() ; i++) {
+			
 			if((sb.charAt(i) == '-' || sb.charAt(i) == '_') && Character.isLowerCase(sb.charAt(i+1)) ) {
 				sb.setCharAt(i+1, (char) (sb.charAt(i+1)-32));
 			}
@@ -19,5 +21,6 @@ public class ConvertStringToCamelCase {
 		
 		return sb.toString().replaceAll("_", "").replaceAll("-", "");;
 	}
+	
 
 }

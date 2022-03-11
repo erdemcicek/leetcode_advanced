d34fa05514e10732804edd602fb1bde042ef8a88
erdemcicek <79759792+erdemcicek@users.noreply.github.com>
2022-03-11 01:17:42 +0100

    Update GenerateParanthesis.java

--- src/backtracking/GenerateParanthesis.java
+++ src/backtracking/GenerateParanthesis.java
@@ -8,7 +8,9 @@ import java.util.List;
 public class GenerateParanthesis {
 	
 	public static void main(String[] args) {
+		
 //		System.out.println(generateParenthesis(3));
+		
 	}
 	
 	private static List<String> stackGeneratePar(int n){
